d0e56e348394282be107f7c2bb36b650fd26d845
kacey_c <kaceycreative@gmail.com>
2021-06-28 11:00:35 -0400

    Update to css folders

--- wp-content/themes/Divi-Child-Theme/functions.php
+++ wp-content/themes/Divi-Child-Theme/functions.php
@@ -1,9 +1,11 @@
 <?php
-function divi__child_theme_enqueue_styles() {
+function wpb_add_google_fonts() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'dove-yoga-child-google-fonts', '//fonts.googleapis.com/css2?family=Calligraffitti&family=Raleway:ital,wght@0,300;0,400;1,300;1,400&display=swap');
-}
-    add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
+ 
+    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Calligraffitti&family=Raleway:ital,wght@0,300;0,400;1,300;1,400&display=swap', false ); 
+    }
+     
+    add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
  
  
 //you can add custom functions below this line:
