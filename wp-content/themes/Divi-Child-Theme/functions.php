871a365c3f637d44aa5df4d14b65f53091c80716
kacey_c <kaceycreative@gmail.com>
2021-07-17 12:44:13 -0400

    google iframe and img added

--- wp-content/themes/Divi-Child-Theme/functions.php
+++ wp-content/themes/Divi-Child-Theme/functions.php
@@ -2,7 +2,7 @@
 function wpb_add_google_fonts() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
-    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Calligraffitti&family=Raleway:ital,wght@0,300;0,400;1,300;1,400&display=swap', false ); 
+    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Calligraffitti&family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap', false ); 
     }
      
     add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
